c77086e97d0776ff52b8cda0b679be79ba7faed2
yurikrupnik <krupnik.yuri@gmail.com>
2021-09-07 22:24:26 +0300

    removed deploy npm and added 2 func2

---
+++ apps/big-data/publish-topic/src/main.ts
@@ -0,0 +1,24 @@
+import { PubSub } from "@google-cloud/pubsub";
+import type { Request, Response } from "express";
+const pubsub = new PubSub();
+
+type events = "be-1" | "agent-1" | "fe-s1" | "d-s" | "be_logs";
+
+function publishPubSubMessage(topic: events, message: any) {
+  const buffer = Buffer.from(JSON.stringify(message));
+  return pubsub.topic(topic).publish(buffer);
+}
+
+const publishTopic = (req: Request, res: Response) => {
+  const { topic } = req.body;
+  delete req.body.topic;
+  publishPubSubMessage(topic, req.body)
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((err) => {
+      console.log("Failed send PubSub topic", err); // eslint-disable-line
+    });
+};
+
+export { publishTopic };
